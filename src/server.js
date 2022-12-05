170495f2dde01cb8e428ac66cd7241fbccc6a063
Thanh Nguyen <thanh.nguyen@teamrg.com>
2022-12-05 15:32:09 -0500

    refactor route

--- src/server.js
+++ src/server.js
@@ -1,10 +1,13 @@
 import express from 'express';
+import { routes } from './routes';
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-	res.status(200).json({ name: 'Thanh' });
-})
+app.use(express.json());
+
+routes.forEach(route => {
+	app[route.method](route.path, route.handler);
+});
 
 app.listen(8080, () => {
 	console.log('Server is listening on port 8080');
