9d742c9b215ed922433f49a2be2da7f0fcbabc9f
Thanh Nguyen <thanh.nguyen@teamrg.com>
2022-12-05 15:20:14 -0500

    add basic endpoint

--- src/server.js
+++ src/server.js
@@ -2,6 +2,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/hello', (req, res) => {
+	res.send('Hello');
+})
+
 app.listen(8080, () => {
 	console.log('Server is listening on port 8080');
 })
