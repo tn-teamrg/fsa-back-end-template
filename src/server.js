b16265c20ee92d394f299736f25267f2eccfe863
Thanh Nguyen <thanh.nguyen@teamrg.com>
2022-12-05 15:49:14 -0500

    add mongodb; add mongodb config

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { routes } from './routes';
+import { db } from './db';
 
 const app = express();
 
@@ -9,6 +10,10 @@ routes.forEach(route => {
 	app[route.method](route.path, route.handler);
 });
 
-app.listen(8080, () => {
-	console.log('Server is listening on port 8080');
-})
+const start = async () => {
+	await db.connect('mongodb://localhost:27017');
+	await app.listen(8080);
+	console.log('Server is listening on port 8080')
+}
+
+start();
